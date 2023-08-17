aa45c294ff9a5a522f1496e7d029b2cd8d34be7e
Wachira Xanders <muhindia66@gmail.com>
2023-08-17 14:54:03 -0700

    This is the debugging session no 1

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+this readme file contains all that you need to know to succeed in all the debugging projects that you will be requested to do.
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *positive_or_negative - Gives the correct output
+ *@i: The letter being inspected.
+ *
+ *Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
