6b9962a84bda7efb2460e882f6f5408f2bd7699f
Wachira Xanders <muhindia66@gmail.com>
2023-08-18 19:34:25 -0700

    The multiplication of values a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *mul - multiplies the two values a and b.
+ *@a: One of the numbers being multiplied.
+ *@b: The other number being multiplied.
+ *
+ *Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
