43b48e7f410acd830878400056a55dc7e1b0725a
Wachira Xanders <muhindia66@gmail.com>
2023-08-18 19:12:53 -0700

    The ASCII values of the numerical form should also be enclosed in the single quotation marks

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_isdigit - Check for a digit.
+ *@c: Is the digit being checked.
+ *
+ *Return: Returns 1 if c is a digit .Otherwise 0.
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
