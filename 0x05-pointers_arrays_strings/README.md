7f96e8471a740e1bf03e24dbb27690dee1900fcb
Wachira Xanders <muhindia66@gmail.com>
2023-08-21 14:15:38 -0700

    The pointers being updated

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The following readme is detailed to talk about the project arrays and pointers.
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+void reset_to_98(int *n);
+
+int _putchar(char c);
+
+#endif /* _MAIN_H */
