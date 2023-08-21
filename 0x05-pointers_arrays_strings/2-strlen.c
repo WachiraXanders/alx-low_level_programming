6621257825c97752e2d3c7c26346b8c3d60f9396
Wachira Xanders <muhindia66@gmail.com>
2023-08-21 15:05:00 -0700

    The strlen command

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+	*_strlen - Calculates the number of characters.
+	*@s: Is the variable storage of the above.
+	*
+	*Return: Always 0.
+	*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return len;
+}
