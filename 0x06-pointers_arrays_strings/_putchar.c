3b9efebf2b13351288716c76f57bad07ea09a21c
Wachira Xanders <muhindia66@gmail.com>
2023-08-24 04:29:18 -0700

    strcat function.

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+	*_putchar - Will implement the putchar.
+	*@c: Will be the character being checked.
+	*
+	*Return: Standard output (1).
+	*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char *);
+
+char *_strcat(char *dest, char *src);
+
+#endif /* _MAIN_H_ */
