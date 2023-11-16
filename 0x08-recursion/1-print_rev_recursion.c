715b4d716f0d46d00dc1fa9a9bdbe14b18d9095a
Xanders Wachira <muhindia66@gmail.com>
2023-11-15 19:37:21 -0800

    segmentation fault.

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include "main.h"
 #include <string.h>
+/**
+	*_reverse_string_recursion - Recursive helper function to reverse a string.
+	*@s: The string to be reversed.
+	*@start: The starting index.
+	*@end: The ending index.
+	*/
+void _reverse_string_recursion(char *s, int start, int end)
+{
+	if (start < end)
 
+	{
+
+		char temp = s[start];
+
+		s[start] = s[end];
+
+		s[end] = temp;
+
+		_reverse_string_recursion(s, start + 1, end - 1);
+	}
+}
 /**
 	*_print_rev_recursion - will print a string in reverse.
 	*@s: This is the string being reversed.
@@ -11,22 +31,13 @@
 void _print_rev_recursion(char *s)
 {
 	int length = strlen(s);
-	int start = 0;
-	int end = length - 1;
 
-	while (start < end)
+	if (length > 0)
+
 	{
-		char temp = s[start];
-		s[start] = s[end];
-		s[end] = temp;
-
-		start++;
-		end--;
-	}
-
-	while (*s != '\0')
-	{
+		_reverse_string_recursion(s, 0, length - 1);
 		_putchar(*s);
-		s++;
+		_print_rev_recursion(s + 1);
+
 	}
 }
