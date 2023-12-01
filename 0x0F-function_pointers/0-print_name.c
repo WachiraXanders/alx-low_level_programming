f5fb59c0601ce0ea83d3f7308ee5a6368019bf3c
Xanders Wachira <muhindia66@gmail.com>
2023-12-01 11:46:48 -0800

    the 1st quiz

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+	*print_name - Prints a name using a given printing function.
+	*@name: The name to be printed.
+	*@f: A function pointer that specifies the printing format.
+	*
+	*Return: void
+	*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	f(name);
+}
