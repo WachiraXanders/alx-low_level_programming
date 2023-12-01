fb1f7586e8be5dc41094f9861536ff9e9990440a
Xanders Wachira <muhindia66@gmail.com>
2023-12-01 12:20:03 -0800

    the 2nd quiz

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,5 +6,7 @@
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* MAIN_H */
