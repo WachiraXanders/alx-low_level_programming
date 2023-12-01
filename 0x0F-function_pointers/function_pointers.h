bb1e7723cee84ad6d2b5dbf349d0c976ad58e5c5
Xanders Wachira <muhindia66@gmail.com>
2023-12-01 13:06:19 -0800

    third quiz.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
